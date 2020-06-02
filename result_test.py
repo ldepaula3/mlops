3220c0421d5f14b548af0cfeca8c62b7eccb2b81
Lucas de Paula <lucasdepaula3@gmail.com>
2020-06-02 16:22:52 -0400

    results right

--- result_test.py
+++ result_test.py
@@ -1,8 +1,8 @@
 from petshop_model import *
 
-#res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist", 3)
+res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist", 3)
 
-res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist")
+#res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist")
     
 if res not in [0.0, 1.0]:
     raise Exception('Error executing program')
