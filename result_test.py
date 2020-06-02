aa504714eb5efd684afd57a822519173bf922b13
Lucas de Paula <lucasdepaula3@gmail.com>
2020-06-02 16:22:17 -0400

    changing_tests

--- result_test.py
+++ result_test.py
@@ -1,6 +1,7 @@
 from petshop_model import *
 
 #res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist", 3)
+
 res = scoreModel("Adult", "Dog", "Gold Fur", "Normal", "No", "Winter", "Male", "Sterilized at Intake", "Public Assist")
     
 if res not in [0.0, 1.0]:
