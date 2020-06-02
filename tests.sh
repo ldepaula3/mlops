3e68c256e649278aba8cb1de1ab9b2f8e116fc8c
Lucas de Paula <lucasdepaula3@gmail.com>
2020-06-02 14:26:47 -0400

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python petshop_model.py "Adult" "Dog" "Gold Fur" "Normal" "No" "Winter" "Male" "Sterilized at Intake" "Public Assist" 3
+python3 petshop_model.py "Adult" "Dog" "Gold Fur" "Normal" "No" "Winter" "Male" "Sterilized at Intake" "Public Assist" 3
